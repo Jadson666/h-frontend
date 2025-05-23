d3d8a107f7fc615dd32b023c89ba9e69160196d6
Jadson666 <amigo_brian@hotmail.com>
2025-05-23 15:04:35 +0800

    feat: 🎸 init commit for vite react-ts

---
+++ src/ws.ts
@@ -0,0 +1,41 @@
+export interface WsResponse {
+  id: string
+  source: string
+  headline: string
+  assets: string[]
+  link: string
+  keywords: string[]
+  timestamp: number
+  priority: string
+}
+
+const reconnectInterval = 1000
+let timer: number | null = null
+export const connectWs = (props?: { onmessage?: (data: WsResponse) => void }) => {
+  const ws = new WebSocket('ws://localhost:8080');
+
+  ws.onopen = () => {
+    console.log('Connected to server');
+    ws.send('hello');
+    if (timer) {
+      clearInterval(timer)
+      timer = null
+    }
+  };
+
+  ws.onmessage = (event) => {
+    try {
+      props?.onmessage?.(JSON.parse(event.data))
+    } catch (e) {
+      console.log('parse fail', e)
+    }
+  };
+
+  ws.onclose = () => {
+    if (!timer) timer = setInterval(connectWs, reconnectInterval);
+  };
+
+  ws.onerror = (error) => {
+    console.error('WebSocket error:', error);
+  };
+};
